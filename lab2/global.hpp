a573f78204202ec5c75b8c56cc7b6d43f7cf1f7c
UnnamedOrange <lycheng1215@sina.com>
2022-10-29 13:18:11 +0800

    Create lab2

---
+++ lab2/global.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <filesystem>
+
+namespace global
+{
+    // Configurations.
+    inline std::filesystem::path working_dir;
+} // namespace global
---
+++ lab2/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include <global.hpp>
+
+#define WORKING_DIR_FOR_DEBUG R"(D:\Libraries\Downloads)"
+
+int main(int argn, char** argv)
+{
+    if (argn > 1)
+        global::working_dir = argv[1];
+    else
+    {
+#ifndef WORKING_DIR_FOR_DEBUG
+        std::cout << "No working directory specified. Input one: " << std::endl;
+        std::string dir;
+        std::getline(std::cin, dir);
+        global::working_dir = dir;
+#else
+        global::working_dir = WORKING_DIR_FOR_DEBUG;
+#endif
+    }
+    std::cout << "Use " << global::working_dir << " as working directory."
+              << std::endl;
+
+    bool success = false;
+    do
+    {
+        success = true;
+    } while (0);
+    if (!success)
+        return 1;
+}
